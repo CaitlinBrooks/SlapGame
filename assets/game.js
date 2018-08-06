d7bd84b570041ea84799e992e720e653608eea31
CaitlinCodes <caiit@me.com>
2018-08-05 21:27:18 -0600

    mods

--- assets/game.js
+++ assets/game.js
@@ -6,9 +6,9 @@ let target = {
     'KO'
   ],
   moodImgs: [
-    'assets/mario.png',
-    'assets/mariodefending.png',
-    'assets/gameover.jpg'
+    'assets/images/mario.png',
+    'assets/images/mariodefending.png',
+    'assets/images/gameover.jpg'
   ],
   health: 100,
   hits: 0,
@@ -23,15 +23,20 @@ let target = {
 //     'KO'
 //   ],
 //   moodImgs: [
-//     'assets/',
-//     'assets/',
-//     'assets/'
+//     'assets/images/luigi.png',
+//     'assets/images/luigidefending.png',
+//     'assets/images/gameover.jpg'
 //   ],
 //   health: 100,
 //   hits: 0,
 //   moodIndex: 0,
 //   items: ['spiralSpin']
 // }],
+
+// 
+// let activePlayer = target.name.mario
+
+// let players = [mario, luigi]
 
 let items = {
   spiralSpin: { name: 'spiralSpin', modifier: 4, description: 'SPIN!' },
@@ -51,6 +56,24 @@ let hits = document.getElementById('playerHits')
 //   }
 //   return 
 // }
+
+function spin() {
+  target.health = target.health += 4
+  playerHealth.innerHTML = target.health.toString()
+  // target.health -= damage + addMods();
+}
+
+function capThrow() {
+  target.health = target.health += 6
+  playerHealth.innerHTML = target.health.toString()
+  // target.health -= damage + addMods();
+}
+
+function starCoins() {
+  target.health = target.health += 2
+  playerHealth.innerHTML = target.health.toString()
+  // target.health -= damage + addMods();
+}
 
 function slap() {
   target.health = target.health -= 1
@@ -79,14 +102,6 @@ function kick() {
   playerName.innerText = target.name
 }
 
-function spiralSpin() {
-  target.health = target.health += 1
-  playerHealth.innerHTML = target.health.toString()
-  // target.health -= damage + addMods();
-  // target.hits = target.hits += 1
-  // hits.innerHTML = target.hits.toString()
-  playerName.innerText = target.name
-}
 
 function update() {
   hits = document.getElementById('playerHits')
